b703f25e74038038f076dccfe20e672796f35c22
andre-axt <andreaxtbachtold@gmail.com>
2026-05-26 14:32:49 -0300

    Expand - ALU inputs and outputs to 8 bits

--- src/alu.v
+++ src/alu.v
@@ -1,16 +1,16 @@
 module alu(
 	input clk,
 	input rst,
-	input [3:0] in_x, in_y,
-	input [1:0] in_op,
-	output reg [3:0] axy, bxy, //axy(LSB) bxy(MSB)
+	input [7:0] in_x, in_y,
+	input [2:0] in_op,
+	output reg [7:0] axy, bxy, //axy(LSB) bxy(MSB)
 	output reg c // Carry flag 
 
 );
 
-	wire [4:0] sum, sub;
-	wire [7:0] mul;
-	wire [3:0] and_op, or_op, xor_op;
+	wire [8:0] sum, sub;
+	wire [15:0] mul;
+	wire [7:0] and_op, or_op, xor_op;
 
 	assign sum = {1'b0, in_x} + {1'b0, in_y};
 	assign sub = {1'b0, in_x} - {1'b0, in_y};
@@ -28,34 +28,39 @@ module alu(
 		else begin
 			case (in_op)
 				3'b000:	begin
-					axy <= sum[3:0];
-					bxy <= {3'b000,	sum[4]};
-					c <= sum[4];
+					axy <= sum[7:0];
+					bxy <= {7'b0000000,	sum[8]};
+					c <= sum[8];
 				end
 				3'b001: begin
-					axy <= sub[3:0];
-					bxy <= {3'b000, sub[4]};
-					c <= sub[4];
+					axy <= sub[7:0];
+					bxy <= {7'b0000000, sub[8]};
+					c <= sub[8];
 				end
 				3'b010: begin
-					axy <= mul[3:0];	
-					bxy <= mul[7:4];
-					c <= mul[7:4];
+					axy <= mul[7:0];	
+					bxy <= mul[15:8];
+					c <= 1'b0;
 				end
 				3'b011: begin
 					axy <= and_op;
-					bxy <= 4'b0000;
+					bxy <= 8'b00000000;
 					c <= 1'b0;
 				end
 				3'b100:	begin
 					axy <= or_op;
-					bxy <= 4'b0000;
+					bxy <= 8'b00000000;
 					c <= 1'b0;
 				end
 				3'b101: begin
 					axy <= xor_op;
-					bxy <= 4'b0000;
+					bxy <= 8'b00000000;
 					c <= 1'b0;
+				end
+				default: begin
+					axy <= 0;
+					bxy <= 0;
+					c <= 0;
 				end
 			endcase
 		end
